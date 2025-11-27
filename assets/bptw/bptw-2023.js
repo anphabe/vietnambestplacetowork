e3a7defee167507cd6779b1104bd221dd3c1d9c2
tquoc229 <tquoc229@yahoo.com>
2025-11-27 11:58:28 +0700

    update

--- assets/bptw/bptw-2023.js
+++ assets/bptw/bptw-2023.js
@@ -200,9 +200,7 @@
                 $('.vnbptw-ranking-industry').show();
                 // Filter out companies that are not in the ranking
                 data = data.filter(item => item.showinranking !== 0);
-                data.sort(function (a, b) {
-                    return (a.sortname || a.name).localeCompare(b.sortname || b.name);
-                });
+
                 $("#vnbptw-ranking").empty().json2html({ 'companies': data }, template.grid);
             }
         }
